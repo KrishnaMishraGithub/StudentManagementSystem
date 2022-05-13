7879e378e82a9e834186230b90fcef44b08cf989
Krishna Kant Mishra <kkmishra9559@gmail.com>
2022-05-14 00:07:40 +0530

    commit all

---
+++ StudentManagementSystem/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using StudentManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        StudentManagementSystemEntities1 StudentManagementSystemEntity = new StudentManagementSystemEntities1();
+        
+        // GET: Account
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            StudentManagementSystemEntity.Users.Where(u=>u.username == user.username && u.password == user.password);
+            return View();
+        }
+
+    }
+}
